b883748cd1089511b1239cc497eff2cf48895fe7
Clockwork-Worm <111155552+Clockwork-Worm@users.noreply.github.com>
2022-08-12 19:22:46 -0400

    Create genes.py

---
+++ scripts/genes.py
@@ -0,0 +1,15 @@
+from random import choice, randint
+
+class Genes(object): 
+  name = "Genes"
+  
+  GenderGene = ['OO', 'OB', 'Ob', 'BB', 'Bb', 'bb', 'OY', 'BY', 'bY']
+  AlbinoGene = ['CC', 'Cc', 'Cg', 'cc', 'cg', 'gg']
+  DiluteGene = ['DD', 'Dd', 'dd']
+  SpotGene = ['SS', 'Ss', 'ss']
+  TabbyGene = ['AA', 'AK', 'Aa', 'KK', 'Ka', 'aa']
+  LengthGene = ['LL', 'Ll', 'll']
+  
+
+
+  
